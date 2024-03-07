2e3ed57f86f639fbe82b14b996afc3cee60150ea
Mikhail Posevin <mp@workconsult.com>
2024-03-07 17:06:43 +0300

    Add fishy frontend

---
+++ backend/apps/base.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from apps.v1 import route_blog, route_login
+
+
+app_router = APIRouter()
+
+
+app_router.include_router(
+    route_blog.router, prefix="", tags=[""], include_in_schema=False
+)
+app_router.include_router(
+    route_login.router, prefix="/auth", tags=[""], include_in_schema=False
+)
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from db.session import engine
 from db.base import Base
 from apis.base import api_router
+from apps.base import app_router
 
 
 def include_router(app):
-	app.include_router(api_router)
+    app.include_router(api_router)
+    app.include_router(app_router)
 
 
 def create_tables():
@@ -14,10 +17,15 @@ def create_tables():
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
     include_router(app)
+    configure_staticfiles(app)
     return app
+
+
+def configure_staticfiles(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 app = start_application()
